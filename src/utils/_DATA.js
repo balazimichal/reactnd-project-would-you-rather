76611837a08fa4d133df9679596c19ed581b349f
Michal Balazi <balazi.michal@gmail.com>
2018-08-11 16:07:25 +0200

    feat - adding avatar images to the DATA file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,38 +1,38 @@
 let users = {
-    sarahedo: {
-        id: 'sarahedo',
-        name: 'Sarah Edo',
-        avatarURL: ,
-        answers: {
-            "8xf0y6ziyjabvozdd253nd": 'optionOne',
-            "6ni6ok3ym7mf1p33lnez": 'optionOne',
-            "am8ehyc8byjqgar0jgpub9": 'optionTwo',
-            "loxhs1bqm25b708cmbf3g": 'optionTwo'
-        },
-        questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+  sarahedo: {
+    id: "sarahedo",
+    name: "Sarah Edo",
+    avatarURL: "https://randomuser.me/api/portraits/women/3.jpg",
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": "optionOne",
+      "6ni6ok3ym7mf1p33lnez": "optionOne",
+      am8ehyc8byjqgar0jgpub9: "optionTwo",
+      loxhs1bqm25b708cmbf3g: "optionTwo"
     },
-    tylermcginnis: {
-        id: 'tylermcginnis',
-        name: 'Tyler McGinnis',
-        avatarURL: ,
-        answers: {
-            "vthrdm985a262al8qx3do": 'optionOne',
-            "xj352vofupe1dqz9emx13r": 'optionTwo',
-        },
-        questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+    questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"]
+  },
+  tylermcginnis: {
+    id: "tylermcginnis",
+    name: "Tyler McGinnis",
+    avatarURL: "https://randomuser.me/api/portraits/men/46.jpg",
+    answers: {
+      vthrdm985a262al8qx3do: "optionOne",
+      xj352vofupe1dqz9emx13r: "optionTwo"
     },
-    johndoe: {
-        id: 'johndoe',
-        name: 'John Doe',
-        avatarURL: ,
-        answers: {
-            "xj352vofupe1dqz9emx13r": 'optionOne',
-            "vthrdm985a262al8qx3do": 'optionTwo',
-            "6ni6ok3ym7mf1p33lnez": 'optionOne'
-        },
-        questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
-    }
-}
+    questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"]
+  },
+  johndoe: {
+    id: "johndoe",
+    name: "John Doe",
+    avatarURL: "https://randomuser.me/api/portraits/men/54.jpg",
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionOne"
+    },
+    questions: ["6ni6ok3ym7mf1p33lnez", "xj352vofupe1dqz9emx13r"]
+  }
+};
 
 let questions = {
     "8xf0y6ziyjabvozdd253nd": {
