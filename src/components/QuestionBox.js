70ab1c760cc969be4e26006011da27b42086fc85
Michal Balazi <balazi.michal@gmail.com>
2018-08-17 16:34:28 +0200

    refactor - changing question box to stateless functional component

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -2,45 +2,45 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom";
 
-class QuestionBox extends Component {
+const QuestionBox = (props) => {
 
-
-
-
-    viewPoll = (id) => {
-        this.props.history.push(`questions/${id}`)
+    const viewPoll = (id) => {
+        props.history.push(`questions/${id}`)
     }
 
 
-  render() {
-
-    if(this.props === null) {
+    if (props === null) {
         return <p>This question does not exist.</p>
     }
 
-    const { name, avatar, text, id } = this.props
+    const { name, avatar, text, id } = props
 
     return <div>
         <div className="box-header">
             <h4>{name} asks:</h4>
         </div>
         <div className="box-content">
-          <div className="box-left">
-            <img src={avatar} alt={name} className="avatar" />
-          </div>
-          <div className="box-right">
-            <h4>Would you rather...</h4>
-            <p>
-              ...
+            <div className="box-left">
+                <img src={avatar} alt={name} className="avatar" />
+            </div>
+            <div className="box-right">
+                <h4>Would you rather...</h4>
+                <p>
+                    ...
               {text}
-              ...
+                    ...
             </p>
-                <button onClick={(e) => this.viewPoll(id)}>View poll</button>
-          </div>
+                <button onClick={(e) => viewPoll(id)}>View poll</button>
+            </div>
         </div>
-      </div>;
-  }
+    </div>;
 }
+
+
+
+
+
+
 
 
 function mapStateToProps({ questions, users }, {id}) {
