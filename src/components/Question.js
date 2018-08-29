4accce6fe3e947dd7a669da19670f121822b1d0a
Michal Balazi <balazi.michal@gmail.com>
2018-08-29 18:56:03 +0200

    fix - disable button after question submission
    Preventing from multiple submissions to the same poll.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,56 +1,65 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter, Redirect } from 'react-router-dom'
-import { handleAnswerQuestion } from '../actions/shared'
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Redirect } from 'react-router-dom';
+import { handleAnswerQuestion } from '../actions/shared';
+import PropTypes from 'prop-types';
 
 class Question extends Component {
-
   state = {
     option: '',
-    submit: true,
-  }
+    submit: true
+  };
 
-  handleSelection = (option) => {
-    
+  handleSelection = option => {
     this.setState(() => ({
       option,
-      submit: false,
-    }))
-    
-  }
+      submit: false
+    }));
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const { option } = this.state
-    const { dispatch } = this.props
-    dispatch(handleAnswerQuestion(this.props.match.params.question_id, option))
-    this.props.history.push(`/questions/${this.props.match.params.question_id}`);
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ submit: true });
+    const { option } = this.state;
+    const { dispatch } = this.props;
+    dispatch(handleAnswerQuestion(this.props.match.params.question_id, option));
+    this.props.history.push(
+      `/questions/${this.props.match.params.question_id}`
+    );
+  };
 
   getPercent = (numberVotes, totalVotes) => {
-      let percent = 0
-      if(numberVotes > 0) {
-        percent = Math.round((numberVotes / totalVotes) * 100)
-      }
-      return percent
-  }
+    let percent = 0;
+    if (numberVotes > 0) {
+      percent = Math.round((numberVotes / totalVotes) * 100);
+    }
+    return percent;
+  };
 
   render() {
-    const { authedUser, questions, users} = this.props
-    const question = questions[this.props.match.params.question_id]
-    if (!question) { 
-      return <Redirect to="/404" />
+    const { authedUser, questions, users } = this.props;
+    const question = questions[this.props.match.params.question_id];
+    if (!question) {
+      return <Redirect to="/404" />;
     }
-    const totalVoteNum = question.optionOne.votes.length + question.optionTwo.votes.length;
+    const totalVoteNum =
+      question.optionOne.votes.length + question.optionTwo.votes.length;
 
-    const questionOneVotePercent = this.getPercent(question.optionOne.votes.length, totalVoteNum);
-    const questionTwoVotePercent = this.getPercent(question.optionTwo.votes.length, totalVoteNum);
+    const questionOneVotePercent = this.getPercent(
+      question.optionOne.votes.length,
+      totalVoteNum
+    );
+    const questionTwoVotePercent = this.getPercent(
+      question.optionTwo.votes.length,
+      totalVoteNum
+    );
 
-
-    
-    if (question.optionOne.votes.indexOf(authedUser) !== -1 || question.optionTwo.votes.indexOf(authedUser) !== -1) {
-      return <section>
+    if (
+      question.optionOne.votes.indexOf(authedUser) !== -1 ||
+      question.optionTwo.votes.indexOf(authedUser) !== -1
+    ) {
+      return (
+        <section>
           <div className="wrapper">
             <div className="box">
               <div className="box-header">
@@ -58,51 +67,68 @@ class Question extends Component {
               </div>
               <div className="box-content">
                 <div className="box-left">
-                <img src={users[question.author].avatarURL} alt="" className="avatar" />
+                  <img
+                    src={users[question.author].avatarURL}
+                    alt=""
+                    className="avatar"
+                  />
                 </div>
                 <div className="box-right">
                   <h1>Results</h1>
                   <div className="box">
                     <div className="box-content">
                       <h3>{question.optionOne.text}</h3>
-                      {question.optionOne.votes.indexOf(authedUser) !== -1 ? <span className="choice">
-                          Your choice
-                        </span> : false}
+                      {question.optionOne.votes.indexOf(authedUser) !== -1 ? (
+                        <span className="choice">Your choice</span>
+                      ) : (
+                        false
+                      )}
                       <div className="progressbar-wapper">
-                        <div className="progressbar" style={{ width: `${questionOneVotePercent}%` }}>
+                        <div
+                          className="progressbar"
+                          style={{ width: `${questionOneVotePercent}%` }}
+                        >
                           <span className="progressbar-value">
                             {questionOneVotePercent} %
                           </span>
                         </div>
                       </div>
-                    {question.optionOne.votes.length} out of {totalVoteNum} {totalVoteNum > 1 ? "votes" : "vote"}
+                      {question.optionOne.votes.length} out of {totalVoteNum}{' '}
+                      {totalVoteNum > 1 ? 'votes' : 'vote'}
                     </div>
                   </div>
                   <div className="box">
                     <div className="box-content">
                       <h3>{question.optionTwo.text}</h3>
-                      {question.optionTwo.votes.indexOf(authedUser) !== -1 ? <span className="choice">
-                          Your choice
-                        </span> : false}
+                      {question.optionTwo.votes.indexOf(authedUser) !== -1 ? (
+                        <span className="choice">Your choice</span>
+                      ) : (
+                        false
+                      )}
                       <div className="progressbar-wapper">
-                        <div className="progressbar" style={{ width: `${questionTwoVotePercent}%` }}>
+                        <div
+                          className="progressbar"
+                          style={{ width: `${questionTwoVotePercent}%` }}
+                        >
                           <span className="progressbar-value">
                             {questionTwoVotePercent} %
                           </span>
                         </div>
                       </div>
-                    {question.optionTwo.votes.length} out of {totalVoteNum} {totalVoteNum > 1 ? "votes" : "vote"}
+                      {question.optionTwo.votes.length} out of {totalVoteNum}{' '}
+                      {totalVoteNum > 1 ? 'votes' : 'vote'}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>;
+        </section>
+      );
     }
-    
 
-    return <section>
+    return (
+      <section>
         <div className="wrapper">
           <div className="box">
             <div className="box-header">
@@ -110,16 +136,32 @@ class Question extends Component {
             </div>
             <div className="box-content">
               <div className="box-left">
-                <img src={users[question.author].avatarURL} alt="" className="avatar" />
+                <img
+                  src={users[question.author].avatarURL}
+                  alt=""
+                  className="avatar"
+                />
               </div>
               <div className="box-right">
                 <h1>Would You Rather ...</h1>
                 <form onSubmit={this.handleSubmit}>
                   <label>
-                    <input type="radio" name="options" onChange={() => this.handleSelection("optionOne")} checked={this.state.option === "optionOne"} /> {question.optionOne.text}
+                    <input
+                      type="radio"
+                      name="options"
+                      onChange={() => this.handleSelection('optionOne')}
+                      checked={this.state.option === 'optionOne'}
+                    />{' '}
+                    {question.optionOne.text}
                   </label>
                   <label>
-                    <input type="radio" name="options" onChange={() => this.handleSelection("optionTwo")} checked={this.state.option === "optionTwo"} /> {question.optionTwo.text}
+                    <input
+                      type="radio"
+                      name="options"
+                      onChange={() => this.handleSelection('optionTwo')}
+                      checked={this.state.option === 'optionTwo'}
+                    />{' '}
+                    {question.optionTwo.text}
                   </label>
                   <button disabled={this.state.submit}>Submit</button>
                 </form>
@@ -127,15 +169,15 @@ class Question extends Component {
             </div>
           </div>
         </div>
-      </section>;
-
+      </section>
+    );
   }
 }
 
 Question.propTypes = {
   questions: PropTypes.object.isRequired,
   users: PropTypes.object.isRequired,
-  authedUser: PropTypes.string.isRequired,
+  authedUser: PropTypes.string.isRequired
 };
 
 function mapStateToProps({ questions, users, authedUser }) {
@@ -143,7 +185,7 @@ function mapStateToProps({ questions, users, authedUser }) {
     authedUser,
     questions,
     users
-  }
+  };
 }
 
 export default withRouter(connect(mapStateToProps)(Question));
