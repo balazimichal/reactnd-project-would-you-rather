e323d0c088ae7861d2ce6e76112ed6a6a3762412
Michal Balazi <balazi.michal@gmail.com>
2018-08-30 18:55:16 +0200

    fix - clearing out warning messages
    Getting rid of Comonent from imports.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,6 +7,20 @@ const Header = ({ authedUser, users }) => (
     <nav className="purple darken-3">
       <div className="container">
         <div className="nav-wrapper">
+          <a data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul className="sidenav" id="mobile-demo">
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/add">NEW QUESTION</Link>
+            </li>
+            <li>
+              <Link to="/leaderboard">LEADERBOARD</Link>
+            </li>
+          </ul>
           <ul className="right">
             <li>
               {authedUser === null ? (
@@ -20,7 +34,9 @@ const Header = ({ authedUser, users }) => (
                     src={users[authedUser].avatarURL}
                     alt=""
                   />
-                  {users[authedUser].name}
+                  <span className="hide-on-med-and-down">
+                    {users[authedUser].name}
+                  </span>
                   <Link className="waves-effect waves-light btn" to="/logout">
                     LOGOUT
                     <i className="material-icons right">account_circle</i>
